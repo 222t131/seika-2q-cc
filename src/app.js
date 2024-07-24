2c7e1f7fc71d9152506d1f71ccd3486aa13f6692
nakahashi <n222t131@st.kyoto-seika.ac.jp>
2024-07-25 01:03:30 +0900

    app.htmlの編集

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("aaa")
+function makeReservation() {
+    const timeInput = document.getElementById('time').value;
+    if (timeInput) {
+        const reservationTime = dayjs().startOf('day').add(dayjs.duration(timeInput)).format('HH:mm');
+        document.getElementById('result').textContent = `予約時間: ${reservationTime}`;
+    } else {
+        document.getElementById('result').textContent = '時間を選択してください。';
+    }
+}
